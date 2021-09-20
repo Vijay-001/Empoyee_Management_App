02e0cff66d4696615a82eb85b2920cf3396c5f43
Vijay <Vijaykulkarni43@gmail.com>
2021-09-20 15:41:18 +0530

    Modifiy coverage code and jest configuration

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,3 @@
-import '@testing-library/jest-dom';
 import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-Enzyme.configure({ adapter: new Adapter() });
+import ReactSixteenAdapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new ReactSixteenAdapter() });
